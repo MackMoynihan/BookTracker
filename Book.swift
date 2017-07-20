3091e6524266ed536c0bf396fe50c14b7caf7506
Mack Moynihan <mdm133@pitt.edu>
2017-07-20 11:33:46 -0400

    initial commit
    added view controllers and segues

---
+++ Book.swift
@@ -0,0 +1,12 @@
+//
+//  Book.swift
+//  BookTracker
+//
+//  Created by Mack Moynihan on 7/20/17.
+//  Copyright © 2017 Mack Moynihan. All rights reserved.
+//
+
+import Foundation
+class Book {
+    
+}
